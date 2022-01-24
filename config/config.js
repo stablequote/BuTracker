70be860b2271ae75611f974110eacee59ea2180a
Esat <66972689+stablequote@users.noreply.github.com>
2022-01-24 03:23:48 +0300

    Delete config directory

--- config/config.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-  env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 3000,
-  jwtSecret: process.env.JWT_SECRET,
-  DB_URL: process.env.MONGODB_URI
-}
-
-// export default config
